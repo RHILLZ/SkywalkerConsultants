e9c9da0b520351fe03b176bd7144ac9e6c0299a1
Richard Hill <rhillx.code@gmail.com>
2020-12-20 13:14:02 -0500

    added mobile menu responsive

--- app.js
+++ app.js
@@ -50,33 +50,138 @@
      subscribe.addEventListener('click', () => {
          const subscribersEmail = email.value;
 
-         docRef.update({ emails: firebase.firestore.FieldValue.arrayUnion(subscribersEmail) });
-
-         console.log("Done adding email to list!");
+        docRef.update({ emails: firebase.firestore.FieldValue.arrayUnion(subscribersEmail) });
+        
+        email.style.display ='none';
+        subscribe.style.display = 'none';
+        text.innerHTML = 'Thank You for Subscribing!'
+        
      })
 
 
      var showInfo = document.querySelectorAll(".show-info");
-     var infoNodeList = document.querySelectorAll('.mobile-services-info');
+     var infoNodeList = document.querySelectorAll('.service');
      const rollovers = infoNodeList[0];
      const longTerm = infoNodeList[1];
+     const collegePlan = infoNodeList[2];
+     const lifeInsurance = infoNodeList[3];
+     const legacyPlan = infoNodeList[4];
+     const willsAndTrust = infoNodeList[5];
 
 
-     var icon;
-     var i;
-     // console.log(showInfo.length);
+     
 
-     for (i = 0; i < 6; i++) {
-         showInfo[i].addEventListener('click', (e) => {
-             //  infoNodeList[i].classList.contains('info-hide') ? infoNodeList[i].classList.remove('info-hide') : infoNodeList[i].classList.add('info-hide');
-             console.log(i);
-         })
+     rollovers.addEventListener('click', ()=>{
+        const icon = rollovers.children[0].children[1];
+         if(rollovers.children[1].classList.contains('info-hide')){
 
-     }
+             rollovers.children[1].classList.remove('info-hide');
+             icon.style.transform = "rotate(180deg)";
+            }else{
+                
+                rollovers.children[1].classList.add('info-hide');
+                icon.style.transform = "rotate(0deg)";
+         }
+     })
+     longTerm.addEventListener('click', ()=>{
+        const icon = longTerm.children[0].children[1];
+         if(longTerm.children[1].classList.contains('info-hide')){
+
+             longTerm.children[1].classList.remove('info-hide');
+             icon.style.transform = "rotate(180deg)";
+            }else{
+                
+                longTerm.children[1].classList.add('info-hide');
+                icon.style.transform = "rotate(0deg)";
+         }
+     })
+     collegePlan.addEventListener('click', ()=>{
+        const icon = collegePlan.children[0].children[1];
+         if(collegePlan.children[1].classList.contains('info-hide')){
+
+             collegePlan.children[1].classList.remove('info-hide');
+             icon.style.transform = "rotate(180deg)";
+            }else{
+                
+                collegePlan.children[1].classList.add('info-hide');
+                icon.style.transform = "rotate(0deg)";
+         }
+     })
+     lifeInsurance.addEventListener('click', ()=>{
+        const icon = lifeInsurance.children[0].children[1];
+         if(lifeInsurance.children[1].classList.contains('info-hide')){
+
+             lifeInsurance.children[1].classList.remove('info-hide');
+             icon.style.transform = "rotate(180deg)";
+            }else{
+                
+                lifeInsurance.children[1].classList.add('info-hide');
+                icon.style.transform = "rotate(0deg)";
+         }
+     })
+     legacyPlan.addEventListener('click', ()=>{
+        const icon = legacyPlan.children[0].children[1];
+         if(legacyPlan.children[1].classList.contains('info-hide')){
+
+             legacyPlan.children[1].classList.remove('info-hide');
+             icon.style.transform = "rotate(180deg)";
+            }else{
+                
+                legacyPlan.children[1].classList.add('info-hide');
+                icon.style.transform = "rotate(0deg)";
+         }
+     })
+     willsAndTrust.addEventListener('click', ()=>{
+         const icon = willsAndTrust.children[0].children[1];
+         console.log(icon);
+         if(willsAndTrust.children[1].classList.contains('info-hide')){
+
+             willsAndTrust.children[1].classList.remove('info-hide');
+            
+            icon.style.transform = "rotate(180deg)";
+        }else{
+            
+            willsAndTrust.children[1].classList.add('info-hide');
+            
+                icon.style.transform = "rotate(0deg)";
+         }
+     })
 
 
 
+const hamburger = document.getElementById('hamburger');
+const close = document.getElementById('close');
+const mobileNav = document.querySelector('.mobile-nav');
 
+hamburger.addEventListener('click',()=>{
+    // hamburger.style.transform = "rotate(360deg)";
+    // hamburger.style.opacity = '0';
+    hamburger.classList.toggle('show');
+    // close.style.transform = "rotate(-360deg)";
+    // close.style.opacity = '1';
+    close.classList.toggle('show');
+    mobileNav.classList.toggle('mobile-nav-active');
+    
+})
+
+close.addEventListener('click', ()=>{
+    close.classList.toggle('show');
+    hamburger.classList.toggle('show');
+    mobileNav.classList.toggle('mobile-nav-active');
+})
+
+const navLinks = document.querySelectorAll('a');
+
+for(var navLink of navLinks){
+
+    
+    navLink.addEventListener('click', ()=>{
+        close.classList.toggle('show');
+        hamburger.classList.toggle('show');
+        mobileNav.classList.toggle('mobile-nav-active');
+    })
+}
+    
 
 
 
